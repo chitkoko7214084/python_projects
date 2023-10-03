d140d9ec0fca932ba3241ebacefc2220490a05b2
chit ko <80560570+chitkoko7214084@users.noreply.github.com>
2023-10-03 12:11:47 -0400

    10/03/2023

---
+++ UserAccount.py
@@ -0,0 +1,28 @@
+class UserAccount:
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+    def createUserAccount(self):
+        pass
+    def restPassword(self):
+        pass 
+    def SendEmail(self):
+        pass
+    
+    
+class UserAccount:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        
+class CreateUser:
+    def createUserAccount(self, user):
+        pass 
+
+class PasswordReset:
+    def restPasswrod(self, user):
+        pass
+    
+class NotifyUser:
+    def sendEmail(self,user):
+        pass
---
+++ staticmethods.py
@@ -0,0 +1,12 @@
+class MathUtilities:
+    @staticmethod
+    def add(x, y):
+        return x +y
+    @staticmethod
+    def subtract(x, y):
+        return x- y
+    @staticmethod
+    def mutiply (x,y):
+        return x * y
+result = MathUtilities.add(5, 3)
+print(result)
